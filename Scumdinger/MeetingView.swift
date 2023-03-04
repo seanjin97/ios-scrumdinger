144a5a05a536d7ed4256ac4bda1d3e2b589820da
Lee Sean Jin <seanjin97@gmail.com>
2023-03-04 12:01:47 +0800

    Initial Commit

---
+++ Scumdinger/MeetingView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Scumdinger
+//
+//  Created by Lee Sean Jin on 4/3/23.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ Scumdinger/ScumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScumdingerApp.swift
+//  Scumdinger
+//
+//  Created by Lee Sean Jin on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ScumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
